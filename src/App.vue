eded2c6a1eda0772f05ffc61ce1b7809893a4d4b
Oldmoon <z5268228@gmail.com>
2023-01-05 13:22:15 +0800

    remove

--- src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="theme-yellow">
-    <router-view/>
-  </div>
-</template>
-
-<style lang="scss">
-@import "@/assets/scss/all.scss";
-</style>
--- src/views/BackendSideBar.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>側邊欄</h1>
-</template>
